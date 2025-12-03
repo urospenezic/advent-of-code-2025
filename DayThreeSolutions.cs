0cbc6886f10f2d965e8abcf8795762122ac4a5d3
urospenezic <uros.penezic996@gmail.com>
2025-12-03 15:21:59 +0100

    Comment for clarification.

--- DayThreeSolutions.cs
+++ DayThreeSolutions.cs
@@ -2,6 +2,29 @@ using System;
 
 namespace MyScript;
 
+/*
+Current segment to evaluate: 1322232245222415222432632212222232132125222522222424251221232212222224137321221142152242222112122421
+Current max digits: 7, remaining segment: 321221142152242222112122421
+Current segment to evaluate: 321221142152242222112122421
+Current max digits: 75, remaining segment: 2242222112122421
+Current segment to evaluate: 2242222112122421
+Current max digits: 754, remaining segment: 2222112122421
+Current segment to evaluate: 2222112122421
+Current max digits: 7542, remaining segment: 222112122421
+Current segment to evaluate: 222112122421
+Current max digits: 75422, remaining segment: 22112122421
+Current segment to evaluate: 22112122421
+Current max digits: 754222, remaining segment: 2112122421
+Current segment to evaluate: 2112122421
+Current max digits: 7542222, remaining segment: 112122421
+Current segment to evaluate: 112122421
+Current max digits: 75422222, remaining segment: 122421
+Current segment to evaluate: 122421
+Current max digits: 754222222, remaining segment: 2421
+Current segment to evaluate: 2421
+Current max digits: 7542222224, remaining segment: 21
+Final max digits for bank segment: 754222222421, attempting parse to long.
+*/
 public static class DayThreeSolutions
 {
     public static long Solve(string input, int bankSize = 12)
